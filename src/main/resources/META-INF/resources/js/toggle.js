35c0b757e8e1ec305e47e16bf71d121868f4a896
jaehuck04 <imjeahyeak24@gmail.com>
2026-06-09 16:22:09 +0900

    리드미 내용 추가

--- src/main/resources/META-INF/resources/js/toggle.js
+++ src/main/resources/META-INF/resources/js/toggle.js
@@ -1,18 +1,30 @@
 // [기존] 다크/라이트 모드 토글 함수
-function toggleTheme() {
+function updateTheme() {
     const body = document.body;
     const btn = document.getElementById('themeToggleBtn');
     const navbar = document.querySelector('.navbar');
 
-    body.classList.toggle('light-mode');
 
     if (body.classList.contains('light-mode')) {
+        sessionStorage.setItem('theme', 'light');
         btn.textContent = 'LIGHT';
         navbar.classList.remove('navbar-dark', 'bg-dark');
         navbar.classList.add('navbar-light', 'bg-light');
     } else {
         btn.textContent = 'DARK';
+        sessionStorage.setItem('theme', 'dark');
         navbar.classList.remove('navbar-light', 'bg-light');
         navbar.classList.add('navbar-dark', 'bg-dark');
     }
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('light-mode');
+
+    updateTheme();
+}
+
+if (sessionStorage.getItem('theme') === 'light') {
+    document.body.classList.add('light-mode');
+    updateTheme();
 }
